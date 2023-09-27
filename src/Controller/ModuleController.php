1c2db3bdc2a9dfd427f3bd4fadccf96cb9b3fa00
Roghan95 <p90svborz@gmail.com>
2023-09-27 11:59:51 +0200

    Ajout modifier et supprimer un module

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Module;
+use App\Form\ModuleType;
 use App\Repository\ModuleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,13 +22,52 @@ class ModuleController extends AbstractController
         ]);
     }
 
-    #[Route('/module/{id}', name: 'show_module')]
-    public function show(Module $module, ModuleRepository $moduleRepository ) : Response {
+    #[Route('/module/new', name: 'new_module')]
+    #[Route('/module/{id}/edit', name: 'edit_module')]
+    public function new_edit(Module $module = null, Request $request = null, EntityManagerInterface $entityMananger) : Response {
+        
+        // Si il existe
+        if(!$module) {
+            // On instancie le manager
+            $module = new Module();
+        }
 
-        // $modulesNotIn = $moduleRepository->findNonProgrammer($module->getId());
+        // On récupère le form de l'entity moduleType
+        $form = $this->createForm(ModuleType::class, $module);
+
+        // on vérifie l'intégrité des données qu'on a reçu par rapport aux données attendus 
+        $form->handleRequest($request);
+
+        // Si il est soumis et valide
+        if($form->isSubmitted() && $form->isValid()) {
+            $module = $form->getData(); // on récupère le saisi du form
+
+            $entityMananger->persist($module); // prepare PDO
+            $entityMananger->flush(); // execute PDO
+
+            return $this->redirectToRoute('app_module');
+        }
+
+        return $this->render('module/new.html.twig', [
+            'formAddModule' => $form
+        ]);
+    }
+
+    // Fonction permettant de supprimer un module
+    #[Route('/module/{id}/delete', name: 'delete_module')]
+    public function delete(Module $module, EntityManagerInterface $entityMananger) {
+        
+        $entityMananger->remove($module); // prepare PDO
+        $entityMananger->flush(); // execute PDO
+
+        return $this->redirectToRoute('app_module');
+    }
+
+    #[Route('/module/{id}', name: 'show_module')]
+    public function show(Module $module) : Response {
+
         return $this->render('session/show.html.twig', [
             'module' => $module
-            // 'modulesNotIn' => $modulesNotIn
         ]);
     }
 }
---
+++ src/Form/ModuleType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Module;
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ModuleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomModule', TextType::class, [
+                'attr' => [
+                    'class' => 'form-module'
+                ]
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'attr' => [
+                    'class' => 'form-module'
+                ]
+            ])
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn-module'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Module::class,
+        ]);
+    }
+}
