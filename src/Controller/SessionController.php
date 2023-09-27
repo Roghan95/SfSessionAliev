a76a93b4089308787a6b818798c7c6a5280b7dd5
Roghan95 <p90svborz@gmail.com>
2023-09-27 14:49:02 +0200

    Ajout de la possibilité d'ajouter une nouvelle session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\Formateur;
+use App\Form\SessionType;
 use App\Repository\SessionRepository;
 use App\Repository\FormateurRepository;
 use App\Repository\ProgrammeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,6 +31,49 @@ class SessionController extends AbstractController
             'programmes' => $programmes
         ]);
     }
+
+    #[Route('/session/new', name: 'new_session')]
+    #[Route('/session/{id}/edit', name: 'edit_session')]
+    public function new_edit(Session $session = null, Request $request = null, EntityManagerInterface $entityMananger) : Response {
+        
+        // Si il existe
+        if(!$session) {
+            // On instancie le manager
+            $session = new Session();
+        }
+
+        // On récupère le form de l'entity sessionType
+        $form = $this->createForm(SessionType::class, $session);
+
+        // on vérifie l'intégrité des données qu'on a reçu par rapport aux données attendus 
+        $form->handleRequest($request);
+
+        // Si il est soumis et valide
+        if($form->isSubmitted() && $form->isValid()) {
+            $session = $form->getData(); // on récupère le saisi du form
+
+            $entityMananger->persist($session); // prepare PDO
+            $entityMananger->flush(); // execute PDO
+
+            return $this->redirectToRoute('app_session');
+        }
+
+        return $this->render('session/new.html.twig', [
+            'formAddSession' => $form
+        ]);
+    }
+
+    // Fonction permettant de supprimer un session
+    #[Route('/session/{id}/delete', name: 'delete_session')]
+    public function delete(Session $session, EntityManagerInterface $entityMananger) {
+        
+        $entityMananger->remove($session); // prepare PDO
+        $entityMananger->flush(); // execute PDO
+
+        return $this->redirectToRoute('app_session');
+    }
+
+
     
 
     #[Route('/session/{id}', name: 'show_session')]
@@ -37,15 +83,15 @@ class SessionController extends AbstractController
         // Récupère les stagiaires qui ne sont pas inscrits à cette session
         $stagiairesNotIn = $sessionRepository->findStagiaireNotIn($session->getId());
     
-        // Récupère les modules qui ne sont pas programmés pour cette session
-        $modulesNotIn = $sessionRepository->findNonProgrammer($session->getId());
+        // Récupère les sessions qui ne sont pas programmés pour cette session
+        $sessionsNotIn = $sessionRepository->findNonProgrammer($session->getId());
     
         // Renvoie la vue 'session/show.html.twig' avec les détails de la session, les stagiaires non inscrits
-        // et les modules non programmés
+        // et les sessions non programmés
         return $this->render('session/show.html.twig', [
             'session' => $session,
             'stagiairesNotIn' => $stagiairesNotIn,
-            'modulesNotIn' => $modulesNotIn
+            'sessionsNotIn' => $sessionsNotIn
         ]);
     }
     
---
+++ src/Form/SessionType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Formateur;
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomSession', TextType::class, [
+                'label' => 'Nom de la session * : ',
+                'attr' => [
+                    'class' => 'form-session',
+                ]
+            ])
+            ->add('nbPlaces', IntegerType::class, [
+                'attr' => [
+                    'class' => 'form-session'
+                ]
+            ])
+
+            ->add('dateDebut', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                   'class' => 'form-session'
+                ]
+            ])
+
+            ->add('dateFin', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                   'class' => 'form-session'
+                ]
+            ])
+
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'attr' => [
+                    'class' => 'form-session'
+                ]
+            ])
+            ->add('formateur', EntityType::class, [
+                'class' => Formateur::class,
+                'attr' => [
+                    'class' => 'form-session'
+                ]
+            ])
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn-session'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
