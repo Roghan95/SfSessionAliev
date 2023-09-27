4c87eb8365cd14ccc5a307a0c321a2f1f53a786d
Roghan95 <p90svborz@gmail.com>
2023-09-27 15:05:11 +0200

    New commit

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -6,7 +6,6 @@ use App\Entity\Session;
 use App\Entity\Formateur;
 use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
-use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +13,6 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class SessionType extends AbstractType
 {
