4dfb7ec5d7da470152f556592ee572be66160354
Roghan95 <p90svborz@gmail.com>
2023-10-02 12:00:35 +0200

    New commit

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Module;
+use App\Form\ModuleType;
 use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,6 +18,7 @@ class CategorieController extends AbstractController
     #[Route('/categorie', name: 'app_categorie')] // URL
     public function index(CategorieRepository $categorieRepository): Response
     {
+        if($this->getUser()) {
         // Récupère toutes les catégories dans l'ordre alphabétique des noms de catégorie
         $categories = $categorieRepository->findBy([], ['nomCategorie' => 'ASC']);
         
@@ -22,8 +27,59 @@ class CategorieController extends AbstractController
             'categories' => $categories,
         ]);
     }
+    return $this->redirectToRoute('app_login');
+    }
+     // Fonction permettant d'ajouter un module et l'éditer
+     #[Route('/categorie/new', name: 'new_categorie')] // URL
+     #[Route('/categorie/{id}/edit', name: 'edit_categorie')] // URL
+     public function new_edit(Categorie $categorie = null, Request $request = null, EntityManagerInterface $entityMananger) : Response {
+         if($this->getUser()) {
+         // Si il existe
+         if(!$categorie) {
+             // On instancie le manager
+             $categorie = new Categorie();
+         }
+ 
+         // On récupère le form de l'entity moduleType
+         $form = $this->createForm(CategorieType::class, $categorie);
+ 
+         // on vérifie l'intégrité des données qu'on a reçu par rapport aux données attendus 
+         $form->handleRequest($request);
+ 
+         // Si il est soumis et valide
+         if($form->isSubmitted() && $form->isValid()) {
+             $categorie = $form->getData(); // on récupère le saisi du form
+ 
+             $entityMananger->persist($categorie); // prepare PDO
+             $entityMananger->flush(); // execute PDO
+ 
+             // Redirection vers la page module
+             return $this->redirectToRoute('app_categorie');
+         }
+ 
+         // Renvoie la vue 'module/new.html.twig' avec le form
+         return $this->render('categorie/new.html.twig', [
+             'formAddCategorie' => $form
+         ]);
+     }
+     return $this->redirectToRoute('app_login');
+     }
+ 
+     // Fonction permettant de supprimer un module
+     #[Route('/categorie/{id}/delete', name: 'delete_categorie')]
+     public function delete(Categorie $categorie, EntityManagerInterface $entityMananger) {
+         if($this->getUser()) {
+         
+         $entityMananger->remove($categorie); // prepare PDO
+         $entityMananger->flush(); // execute PDO
+ 
+         // Redirection vers la page module
+         return $this->redirectToRoute('app_categorie');
+         }
+         return $this->redirectToRoute('app_login');
+     }
 
-    // Fonction permettant d'afficher une catégorie
+    // Fonction permettant d'afficher le détail d'une catégorie
     #[Route('/categorie/{id}', name: 'show_categorie')] // URL
     public function showCategorie(Categorie $categorie) : Response {
         
